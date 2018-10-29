43fc0a0da03326e572df9c1731eb6b076440be66
rizwan atta <rizwanatta890@gmail.com>
2018-10-29 18:10:49 +0500

    mongodb url cleaned

--- utils/constants.js
+++ utils/constants.js
@@ -1,3 +1,3 @@
-// let baseDbUrl = 'mongodb://localhost:27017/servinco_db';
-let baseDbUrl = 'mongodb://<heroku_0vvslq0h>:<Namaz890>@ds243963.mlab.com:43963/heroku_0vvslq0h';
+let baseDbUrl = 'mongodb://localhost:27017/servinco_db';
+let baseDbUrl = 'mongodb://heroku_0vvslq0h:Namaz890@ds243963.mlab.com:43963/heroku_0vvslq0h';
 module.exports = baseDbUrl;
