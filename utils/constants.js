6c57d7dd48e4c23b7144eb5778d5f951f9146d3e
rizwan atta <rizwanatta890@gmail.com>
2018-10-29 19:33:11 +0500

    new db user with read/write acces

--- utils/constants.js
+++ utils/constants.js
@@ -1,3 +1,3 @@
 // let baseDbUrl = 'mongodb://localhost:27017/servinco_db';
-let baseDbUrl = 'mongodb://heroku_0vvslq0h:Namaz890@ds243963.mlab.com:43963/heroku_0vvslq0h?authSource=heroku_0vvslq0h&w=1';
+let baseDbUrl = 'mongodb://rizwan_atta:Namaz890@ds243963.mlab.com:43963/heroku_0vvslq0h?authSource=heroku_0vvslq0h&w=1';
 module.exports = baseDbUrl;
